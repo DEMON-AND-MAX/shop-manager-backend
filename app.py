bc89d8c3a406d44f119e06b8154e63cf6c13508d
DEMON-AND-MAX <demon.and.max@gmail.com>
2024-11-28 21:31:51 +0200

    Add base flask application with three routes to allow frontend testing - '/', '/get', '/post'

---
+++ app.py
@@ -0,0 +1,30 @@
+import flask
+from flask import Flask, request, jsonify
+
+APP_NAME = "Shop Manager Backend"
+APP_DEV = "Amalia Matioc, Maximilian Todea, Ariana Radu"
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def app_base():
+    return "Hello world! " + APP_NAME
+
+@app.route('/get', methods=['GET'])
+def app_get():
+    response = jsonify({
+        "message": "Response from " + APP_NAME + " by " + APP_DEV + "."
+        })
+    return response
+
+@app.route('/post', methods=['POST'])
+def app_post():
+    data = request.get_json()
+    response = jsonify({
+        "message": "Response from " + APP_NAME + " by " + APP_DEV + ".",
+        "data": data,
+    })
+    return response
+
+if __name__ == '__main__':
+    app.run(debug=True)
